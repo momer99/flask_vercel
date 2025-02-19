891f5a92f6cf82782f4dd7c27725f5414d08e7f6
Mohammad Omer Aamir <muhammad01aamir@gmail.com>
2025-02-19 15:33:27 +0500

    updated code

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route("/")
+def home():
+    return "Hello, World!"
 
-# main
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
